fd403cab1358d563d532e5120f376da369e37c32
kenmueller <kenmueller0@gmail.com>
2021-03-22 19:21:46 -0600

    home

--- public/index.js
+++ public/index.js
@@ -2,6 +2,8 @@ const idForm = document.getElementById('id-form')
 const idInput = document.getElementById('id-input')
 const idSubmit = document.getElementById('id-submit')
 
+idInput.focus()
+
 idInput.addEventListener('input', () => {
 	idSubmit.disabled = !idInput.value
 })
